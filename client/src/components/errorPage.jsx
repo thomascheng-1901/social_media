fa223ec37d90757add4892c4b2afc37bb266a8d1
Thomas <93049032+Thomas-HAHA@users.noreply.github.com>
2024-05-13 23:06:24 +0800

    frontend changes

--- client/src/components/errorPage.jsx
+++ client/src/components/errorPage.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const errorPage = () => {
     return (
         <div>
-            <h1>OPPS... Something wrong has occured</h1>
+            <h1>OPPS... Something wrong has occured </h1>
             <Link to = "/"></Link>
         </div>
     )
